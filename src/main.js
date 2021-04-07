61d6d6762f60dc1726f965775f46fa0d0dc75062
wawoyuanli <1583649818@qq.com>
2021-04-07 15:25:17 +0800

    request api

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router/router'
+import store from "./store/index.js";
 /**引入bootstrap 相关文件 */
 import $ from 'jquery'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -15,6 +16,7 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  $ ,
+  $,
+  store,
   render: h => h(App),
 }).$mount('#app')
