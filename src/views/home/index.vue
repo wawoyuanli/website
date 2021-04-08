8f66a49f0ebcffcfdd7436612af200672153160f
wawoyuanli <1583649818@qq.com>
2021-04-08 18:53:25 +0800

    update

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,256 +1,371 @@
 <template>
-  <div class="js home" id="page-top">
-
-    <!--导航组件-->
-    <Nav :navitemlist="navitemlist" />
-    <div class="intro d-flex">
-      <div class="container my-auto">
-        <div class="row">
-          <div class="col-lg-4">
-            <h1 class="page-header mb-3 animated fadeIn" style="font-size:2rem;color:#fff">
-              Earth Money
-              <br> For the things you do online
-            </h1>
-            <p style="font-size:1rem;color:#fff;padding:20px 0px">All-in-one script for earning money online.</p>
-            <div class="joinus-btn"><a  href="/register">Join Us</a></div>
-          </div>
-          <img src="../../assets/images/iphone-x-mockup.png" alt="" style="margin-left:37%;z-index:10">
-        </div>
-
-      </div>
-      <div class="layer" style="margin-top:-160px;width:100%;background: linear-gradient(90deg, #1f75a7 0, #03bec5) !important;;z-index:0;border-top:100px solid transparent; border-right:100vw solid #fff;">
-      </div>
-      <div class="skew skew-right" id="features" style="width:100vw;height:auto;background:#fff">
-
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-12 text-center ">
-              <h2 class="section-heading wow fadeInUp" data-wow-delay="0.1s" style="font-size:2rem;visibility: visible; animation-delay: 0.1s; animation-name: fadeInUp;margin-bottom:3rem;">Easy ways to earn money</h2>
-            </div>
-          </div>
-        </div>
-        <div class="container">
-          <div class="row">
-            <div class="col-md-12">
-              <p class="lead text-center" style="font-size:1.1rem">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="container pt-5 pb-5">
-          <div class="row">
-            <div class="col-lg-4 col-md-6 text-center">
-              <div class="status-box mx-auto mb-3 mb-lg-0">
-                <i class="my-3">图片</i>
-                <h3>
-                  <span class="counter">
-                    16,5000
-                  </span>
-                </h3>
-                <p class="mb-0">users monthly</p>
+	<div class="js home" id="page-top">
+		<!--导航组件-->
+		<Nav :navitemlist="navitemlist" />
+		<div class="intro d-flex">
+			<div class="container my-auto">
+				<div class="row">
+					<div class="col-lg-4">
+						<h1
+							class="page-header mb-3 animated fadeIn"
+							style="font-size: 2rem; color: #fff"
+						>
+							Earth Money
+							<br />
+							For the things you do online
+						</h1>
+						<p style="font-size: 1rem; color: #fff; padding: 20px 0px">
+							All-in-one script for earning money online.
+						</p>
+						<div class="joinus-btn"><a href="/register">Join Us</a></div>
+					</div>
+					<img
+						src="../../assets/images/iphone-x-mockup.png"
+						alt=""
+						style="margin-left: 37%; z-index: 10"
+					/>
+				</div>
+			</div>
+			<div
+				class="layer"
+				style="
+					margin-top: -160px;
+					width: 100%;
+					z-index: 0;
+					border-top: 100px solid transparent;
+					border-right: 100vw solid #fff;
+				"
+			></div>
+			<div
+				class="skew skew-right"
+				id="features"
+				style="width: 100vw; height: auto; background: #fff"
+			>
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-12 text-center">
+							<h2
+								class="section-heading wow fadeInUp"
+								data-wow-delay="0.1s"
+								style="
+									font-size: 2rem;
+									visibility: visible;
+									animation-delay: 0.1s;
+									animation-name: fadeInUp;
+									margin-bottom: 3rem;
+								"
+							>
+								Easy ways to earn money
+							</h2>
+						</div>
+					</div>
+				</div>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<p class="lead text-center" style="font-size: 1.1rem">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+								eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+								enim ad minim veniam, quis nostrud exercitation ullamco laboris
+								nisi ut aliquip ex ea commodo consequat.
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="container pt-5 pb-5">
+					<div class="row">
+						<div class="col-lg-4 col-md-6 text-center">
+							<div class="status-box mx-auto mb-3 mb-lg-0">
+								<i class="my-3"><img src="@/assets/images/earth.png" /></i>
+								<h3>
+									<span class="counter"> $ 16,5000 </span>
+								</h3>
+								<p class="mb-0">users monthly</p>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6 text-center">
+							<div class="status-box mx-auto mb-3 mb-lg-0">
+								<i class="my-3"><img src="@/assets/images/earth.png" /></i>
+								<h3>
+									<span class="counter"> $16,5000 </span>
+								</h3>
+								<p class="mb-0">users monthly</p>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6 text-center">
+							<div class="status-box mx-auto mb-3 mb-lg-0">
+								<i class="my-3"><img src="@/assets/images/earth.png" /></i>
+								<h3>
+									<span class="counter"> $16,5000 </span>
+								</h3>
+								<p class="mb-0">users monthly</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div
+					class="layer"
+					style="
+						width: 100%;
+						z-index: 100;
+						border-top: 100px solid transparent;
+						border-right: 100vw solid rgba(3, 190, 197, 0.9);
+					"
+				></div>
+				<div class="middle">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-6 left">Easy ways to earn money</div>
+							<div class="col-lg-6 right">
+								<p>
+									Companies often need feedback on their products to make sure
+									they’re heading in the right direction; this information can
+									save them a lot of time and money in the long run.
+								</p>
+								<div
+									class="image"
+									style="
+										display: flex;
+										justify-content: space-between;
+										padding: 20px 0px;
+									"
+								>
+									<img src="@/assets/images/earth.png" alt="" />
+									<img src="@/assets/images/earth.png" alt="" />
+									<img src="@/assets/images/earth.png" alt="" />
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div
+					class="layer"
+					style="
+						width: 100%;
+						z-index: 100;
+						border-bottom: 100px solid transparent;
+						border-right: 100vw solid rgb(31, 117, 167);
+					"
+				></div>
+        <div class="map-desc">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-6">
+                <img src="@/assets/images/earth.png" alt="">
+              </div>
+              <div class="col-lg-6">
+                <p>Our Trusted Partners</p>
+                <p>Redeem your points for gift cards to your favorite retailers like Amazon and Netflix or get cash back from PayPal.</p>
               </div>
             </div>
-            <div class="col-lg-4 col-md-6 text-center">
-              <div class="status-box mx-auto mb-3 mb-lg-0">
-                <i class="my-3">图片</i>
-                <h3>
-                  <span class="counter">
-                    16,5000
-                  </span>
-                </h3>
-                <p class="mb-0">users monthly</p>
-              </div>
-            </div>
-            <div class="col-lg-4 col-md-6 text-center">
-              <div class="status-box mx-auto mb-3 mb-lg-0">
-                <i class="my-3">图片</i>
-                <h3>
-                  <span class="counter">
-                    16,5000
-                  </span>
-                </h3>
-                <p class="mb-0">users monthly</p>
-              </div>
-            </div>
           </div>
         </div>
-        <div class="layer" style="width:100%;background: linear-gradient(90deg, #1f75a7 0, #03bec5) !important;;z-index:100;border-top:100px solid transparent; border-right: 100vw solid rgba(3, 190, 197,.2);">
+        		<div
+					class="layer"
+					style="
+						width: 100%;
+						z-index: 100;
+						border-top: 100px solid transparent;
+						border-left: 100vw solid rgb(31, 117, 167)	"
+				></div>
+        <div class="testimonials">
+          <p>Testimonials</p>
+
         </div>
-        <div class="parallax-mirror" style="width:100%;z-index:0">
+				<!-- <div class="parallax-mirror" style="width:100%;z-index:0">
           <img class="parallax-slider" src="../../assets/images/parallax.jpeg" />
-        </div>
-      </div>
+        </div> -->
+			</div>
+		</div>
 
-    </div>
-
-    <Footer />
-  </div>
+		<Footer />
+	</div>
 </template>
 <script>
 import Nav from '@c/nav.vue' //引入导航栏组件
 import Footer from '@c/footer.vue' //引入底部组件
 export default {
-  name: 'Home',
-  components: {
-    Nav: Nav,
-    Footer: Footer,
-  },
-  data () {
-    return {
-      navitemlist: [
-        // { name: 'Home', path: '/', active: 'active' },
-        { name: 'Features', path: '#', active: 'active' },
-        { name: 'support', path: '/support', active: 'active' },
-        { name: 'Contact', path: '/contact', active: 'active' },
-      ],
-    }
-  },
-  mounted () {
-    // a()
-  },
-  methods: {
-    signClick () {
-      this.$router.push('/login')
-    },
-  },
+	name: 'Home',
+	components: {
+		Nav: Nav,
+		Footer: Footer,
+	},
+	data() {
+		return {
+			navitemlist: [
+				// { name: 'Home', path: '/', active: 'active' },
+				{ name: 'Features', path: '#', active: 'active' },
+				{ name: 'support', path: '/support', active: 'active' },
+				{ name: 'Contact', path: '/contact', active: 'active' },
+			],
+		}
+	},
+	mounted() {
+		// a()
+	},
+	methods: {
+		signClick() {
+			this.$router.push('/login')
+		},
+	},
 }
 </script>
  <style scoped  lang="less">
 .home {
-  background: linear-gradient(90deg, #1f75a7 0, #03bec5) !important;
-  color: #343a40;
-  min-height: 100vh;
-  .joinus-btn {
-    border: none;
-    background-color: #ff6f00;
-    border-color: #ff6f00;
-    width: 100px;
-    height: 40px;
-    border-radius: 5px;
-    text-align: center;
-    a{
-      font-size: 1.4rem;
-      color: white;
-   
+	background: linear-gradient(90deg, #1f75a7 0, #03bec5) !important;
+	color: #343a40;
+	min-height: 100vh;
+	.joinus-btn {
+		border: none;
+		background-color: #ff6f00;
+		border-color: #ff6f00;
+		width: 100px;
+		height: 40px;
+		border-radius: 5px;
+		text-align: center;
+		a {
+			font-size: 1.4rem;
+			color: white;
+		}
+	}
+	.parallax-mirror {
+		// visibility: hidden;
+		// z-index: -10;
+		// position: fixed;
+		// top: 0px;
+		// left: 0px;
+		// overflow: hidden;
+		// height: 716px;
+		// width: 1703px;
+		// transform: translate3d(0px, 458px, 0px);
+		.parallax-slider {
+			// position: absolute;
+			// height: 1047px;
+			// width: 1703px;
+			// max-width: none;
+			// transform: translate3d(0px, -633.4px, 0px);
+		}
+	}
+	.intro {
+		display: flex;
+		flex-direction: column;
+		overflow: hidden;
+		min-height: 500px;
+		padding-top: 0px;
+		padding-bottom: 0px;
+		background: linear-gradient(
+			90deg,
+			rgb(31, 117, 167) 0px,
+			rgb(3, 190, 197)
+		) !important;
+		.my-auto {
+			padding-top: 8rem;
+			.row {
+				.col-lg-10 {
+					.mb-3 {
+						margin-bottom: 1rem !important;
+					}
+					.fadeIn {
+						-webkit-animation-name: fadeIn;
+						animation-name: fadeIn;
+					}
+					.animated {
+						-webkit-animation-duration: 1s;
+						animation-duration: 1s;
+						-webkit-animation-fill-mode: both;
+						animation-fill-mode: both;
+					}
+				}
+				.col-lg-8 {
+					.mb-3 {
+						font-size: 1.15rem;
+						margin-bottom: 1rem !important;
+					}
+					.btn-primary {
+						color: #fff !important;
+						background-color: #ff6f00 !important;
+						border-color: #ff6f00 !important;
+					}
+					.btn {
+						border-radius: 5px;
+						font-family: Rubik;
+						display: inline-block;
+						font-weight: 400;
+						line-height: 1.5;
+						color: #ebebeb;
+						text-align: center;
+						text-decoration: none;
+						vertical-align: middle;
+						cursor: pointer;
+						user-select: none;
+						background-color: transparent;
+						border: 1px solid transparent;
+						padding: 0.375rem 0.75rem;
+						font-size: 1rem;
+						border-radius: 0;
+						transition: color 0.15s ease-in-out background-color 0.15s
+								ease-in-out,
+							border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+					}
+					.mr-2 {
+						margin-right: 0.5rem !important;
+					}
+					.mt-3 {
+						margin-top: 1rem !important;
+					}
+					.btn-group-lg .btn .btn-lg {
+						padding: 0.5rem 1rem;
+						font-size: 1.25rem;
+						border-radius: 0;
+					}
+				}
+			}
+		}
+		.middle {
+			width: 100vw;
+			padding: 20px 0px;
+				background: linear-gradient(
+			90deg,
+			rgb(31, 117, 167) 0px,
+			rgb(3, 190, 197)
+		)  ;
+			.left {
+				font-size: 2rem;
+				color: #fff;
+			}
+			.right {
+				color: #fff;
+				font-size: 1.2rem;
+			}
+		}
+    .testimonials{
+      width: 100vw;
+      	background: linear-gradient(
+			90deg,
+			rgb(31, 117, 167) 0px,
+			rgb(3, 190, 197)
+		) !important;
     }
-  }
-  .parallax-mirror {
-    // visibility: hidden;
-    // z-index: -10;
-    // position: fixed;
-    // top: 0px;
-    // left: 0px;
-    // overflow: hidden;
-    // height: 716px;
-    // width: 1703px;
-    // transform: translate3d(0px, 458px, 0px);
-    .parallax-slider {
-      // position: absolute;
-      // height: 1047px;
-      // width: 1703px;
-      // max-width: none;
-      // transform: translate3d(0px, -633.4px, 0px);
-    }
-  }
-  .intro {
-    display: flex;
-    flex-direction: column;
-    overflow: hidden;
-    min-height: 500px;
-    padding-top: 0px;
-    padding-bottom: 0px;
-    background: linear-gradient(
-      90deg,
-      rgb(31, 117, 167) 0px,
-      rgb(3, 190, 197)
-    ) !important;
-    .my-auto {
-      padding-top: 8rem;
-      .row {
-        .col-lg-10 {
-          .mb-3 {
-            margin-bottom: 1rem !important;
-          }
-          .fadeIn {
-            -webkit-animation-name: fadeIn;
-            animation-name: fadeIn;
-          }
-          .animated {
-            -webkit-animation-duration: 1s;
-            animation-duration: 1s;
-            -webkit-animation-fill-mode: both;
-            animation-fill-mode: both;
-          }
-        }
-        .col-lg-8 {
-          .mb-3 {
-            font-size: 1.15rem;
-            margin-bottom: 1rem !important;
-          }
-          .btn-primary {
-            color: #fff !important;
-            background-color: #ff6f00 !important;
-            border-color: #ff6f00 !important;
-          }
-          .btn {
-            border-radius: 5px;
-            font-family: Rubik;
-            display: inline-block;
-            font-weight: 400;
-            line-height: 1.5;
-            color: #ebebeb;
-            text-align: center;
-            text-decoration: none;
-            vertical-align: middle;
-            cursor: pointer;
-            user-select: none;
-            background-color: transparent;
-            border: 1px solid transparent;
-            padding: 0.375rem 0.75rem;
-            font-size: 1rem;
-            border-radius: 0;
-            transition: color 0.15s ease-in-out background-color 0.15s
-                ease-in-out,
-              border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-          }
-          .mr-2 {
-            margin-right: 0.5rem !important;
-          }
-          .mt-3 {
-            margin-top: 1rem !important;
-          }
-          .btn-group-lg .btn .btn-lg {
-            padding: 0.5rem 1rem;
-            font-size: 1.25rem;
-            border-radius: 0;
-          }
-        }
-      }
-    }
-  }
-  header .d-flex {
-    // display: flex !important;
-  }
-  header .my-auto {
-    margin-top: auto !important;
-    margin-bottom: auto !important;
-  }
+	}
 
-  header.intro p {
-    color: rgb(255, 255, 255);
-    font-size: 1.15rem;
-    font-weight: 300;
-  }
-  header.intro .h1,
-  header.intro h1 {
-    font-size: 4rem;
-    color: rgb(255, 255, 255);
-  }
-  header.intro h1 {
-    font-size: 2rem;
-  }
+	header .my-auto {
+		margin-top: auto !important;
+		margin-bottom: auto !important;
+	}
+
+	header.intro p {
+		color: rgb(255, 255, 255);
+		font-size: 1.15rem;
+		font-weight: 300;
+	}
+	header.intro .h1,
+	header.intro h1 {
+		font-size: 4rem;
+		color: rgb(255, 255, 255);
+	}
+	header.intro h1 {
+		font-size: 2rem;
+	}
 }
 </style>
 
