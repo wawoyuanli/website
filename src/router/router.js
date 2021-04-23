335a813222d555e6cdd7f4d5cae1d310bff860bd
wawoyuanli <1583649818@qq.com>
2021-04-23 16:06:16 +0800

    修改路由模式

--- src/router/router.js
+++ src/router/router.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 Vue.use(Router)
 export default new Router({
-  mode: 'history',
+  // mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     /**主页路由 */
