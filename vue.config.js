f04419ad56f3f9d4babfffc22bfa4f566aa1e861
gengxn <984593084@qq.com>
2021-04-28 03:05:20 -0400

    更新生产配置
    更新生产配置

--- vue.config.js
+++ vue.config.js
@@ -64,7 +64,7 @@ module.exports = {
   devServer: {
     open: false, // 编译完成是否打开网页
     host: '0.0.0.0', // 指定使用地址，默认localhost,0.0.0.0代表可以被外界访问
-    port: 8080, // 访问端口
+    port: 9192, // 访问端口
     https: false, // 编译失败时刷新页面
     hot: true, // 开启热加载
     hotOnly: false,
