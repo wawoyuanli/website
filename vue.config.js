09aa11518a40eca73c000361f80b1ac54aba2bcf
gengxn <984593084@qq.com>
2022-04-29 09:44:53 +0800

    增加跨域请求
    增加跨域请求

--- vue.config.js
+++ vue.config.js
@@ -70,7 +70,7 @@ module.exports = {
     hotOnly: false,
     proxy: {
       [process.env.VUE_APP_API]: {
-        target:'https://www.oyasurvey.com',// 'http://www.gentingconsulting.com',////'http://www.surveyleague.club',//"http://g36469v144.zicp.vip", //API服务器的地址  http://g36469v144.zicp.vip
+        target:'https://oyasurvey.com',// 'http://www.gentingconsulting.com',////'http://www.surveyleague.club',//"http://g36469v144.zicp.vip", //API服务器的地址  http://g36469v144.zicp.vip
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_API]: '' // es5
